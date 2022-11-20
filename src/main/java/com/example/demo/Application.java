d943b0a6f3795a55f3a7deee59cd1e6858a7eee5
Vinícius Tonello Rech <vinitrech@pop-os.localdomain>
2022-11-20 13:13:21 -0300

    first and final commit

---
+++ src/main/java/com/example/demo/Application.java
@@ -0,0 +1,23 @@
+package com.example.demo;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+    @Bean
+    CommandLineRunner commandLineRunner(StudentRepository studentRepository) {
+        return args -> {
+            Student maria = new Student("Maria", "Flamingo", "maria@email.com.br", 25);
+            studentRepository.save(maria);
+        };
+    }
+
+}
---
+++ src/main/java/com/example/demo/StudentRepository.java
@@ -0,0 +1,5 @@
+package com.example.demo;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public class StudentRepository extends JpaRepository<Student, Long> {}
